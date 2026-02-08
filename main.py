4f46361433a73e6fa204a75815663914e770077d
Apeksha Malik <malikapeksha@gmail.com>
2026-02-08 12:05:53 -0500

    (fix): add middleware req

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from contextlib import asynccontextmanager
 from src.database.database import connect_to_mongo, close_mongo_connection
 from src.routes.user.user_routes import router as user_router
 from src.routes.session.session_routes import router as session_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -16,6 +17,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="SkillShare Local API", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # allow all origins
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include routers
 app.include_router(user_router, prefix="/users", tags=["Users"])
